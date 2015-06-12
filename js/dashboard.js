6f738996d736de92de5038104a4d2e90202b6b6e
timoline <timoline@gmail.com>
2015-06-12 08:44:08 +0200

    Changed highlighting in dashboard and insights

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,31 @@
-var tmpWatt = 0;
+//var oldvalue= 0;
+var oldjson = 0;
+
 $(document).ready(function() {
+
+	function highlightValue(key,value,oldvalue) {
+		//console.log("NEW: "+value);
+		//console.log("OLD: "+oldvalue);
+		if(oldvalue < value){
+			updown = "countUp";
+			$('#'+key).html("<span class='"+updown+"'>"+(value)+"</span>"); 
+			//$('#'+key).html(value); 
+			$('#'+key).effect( "highlight",{color:"#FF0000"}, 1000 );
+		}
+		else if(oldvalue == value){
+			updown = "";
+			$('#'+key).html("<span class='"+updown+"'>"+(value)+"</span>"); 
+			//$('#'+key).html(value); 
+		}
+		else
+		{
+			updown = "countDown";
+			$('#'+key).html("<span class='"+updown+"'>"+(value)+"</span>");  
+			//$('#'+key).html(value); 
+			$('#'+key).effect( "highlight",{color:"#008000"}, 1000 ); //2C3539
+		}		
+		
+	}
 
     function updateDash(){
         $.ajax({
@@ -8,76 +34,32 @@ $(document).ready(function() {
             cache: false,
             success: function(json) {
             //console.log(json)
-                var currentpowerin = json.p1_current_power_in*1000; // kW -> W
-                
-                if(tmpWatt < parseInt(currentpowerin)){
-                    updown = "countUp";
-                    $('#p1_current_power_in').html("<span class='"+updown+"'>"+(currentpowerin)+"</span>"); 
-                    $("#p1_current_power_in").effect( "highlight",{color:"#FF0000"}, 1000 );
-                }
-                else if(tmpWatt== parseInt(currentpowerin)){
-                    updown = "";
-                    $('#p1_current_power_in').html("<span class='"+updown+"'>"+(currentpowerin)+"</span>"); 
-                }
-                else
-                {
-                    updown = "countDown";
-                    $('#p1_current_power_in').html("<span class='"+updown+"'>"+(currentpowerin)+"</span>");  
-                    $("#p1_current_power_in").effect( "highlight",{color:"#008000"}, 1000 ); //2C3539
-                }
-                tmpWatt = parseInt(currentpowerin);
-                
-                //low - normal tariff
+//******************************************************************************************
+				//convert: kW -> W	
+				json["p1_current_power_in"] = (json["p1_current_power_in"])*1000;		
+								
+                //convert tariff: low - normal
                 var currenttariff = json.p1_current_tariff;
                 if (currenttariff == 1 ) {
-                    rate = "Low";
+                    json["p1_current_tariff"]  = "Low";
                 }
                 else {
-                    rate = "Normal";
-                }                
-                
-                //smartmeter
-                $('#p1_timestamp').html(json.p1_timestamp);  
-                $('#p1_meter_supplier').html(json.p1_meter_supplier);               
-                $('#p1_dsmr_version').html(json.p1_dsmr_version);
-                $('#p1_powerfailures').html(json.p1_powerfailures);
-                
-                //counter
-                $('#p1_current_tariff').html(rate);                
-                $('#p1_meterreading_in_1').html(json.p1_meterreading_in_1); //kWh
-                $('#p1_meterreading_in_2').html(json.p1_meterreading_in_2); //kWh 
-                    
-                //usage
-                $('#p1_instantaneous_current_l1').html(json.p1_instantaneous_current_l1); //A              
+                    json["p1_current_tariff"]  = "Normal";
+                }  
+//******************************************************************************************
+				for (var key in json)
+				{									
+					var value = json[key];
+					var oldvalue = oldjson[key];
 
-                $('#today_min_power').html(json.today_min_power);
-                $('#today_max_power').html(json.today_max_power);
-                $('#today_avg_power').html(json.today_avg_power);
-
-                //today
-                $('#today_date').html(json.today_date);
-                $('#today_normal').html(json.today_normal);
-                $('#today_low').html(json.today_low);
-                $('#today_total').html(json.today_total);    
-                
-                //week
-                $('#week_date').html(json.week_date);
-                $('#week_normal').html(json.week_normal);
-                $('#week_low').html(json.week_low);
-                $('#week_total').html(json.week_total);     
-
-                //month
-                $('#month_date').html(json.month_date);
-                $('#month_normal').html(json.month_normal);
-                $('#month_low').html(json.month_low);
-                $('#month_total').html(json.month_total);    
-
-                //year
-                $('#year_date').html(json.year_date);
-                $('#year_normal').html(json.year_normal);
-                $('#year_low').html(json.year_low);
-                $('#year_total').html(json.year_total);       
+					//$('#'+key).html(json[key]);  
+					highlightValue(key,value,oldvalue); 
+						
+					//console.log($('#'+key).html(json[key]));
+				}
 				
+				oldjson = json;	
+				//console.log("oldjson: "+oldjson);
             }             
         });              
     }
